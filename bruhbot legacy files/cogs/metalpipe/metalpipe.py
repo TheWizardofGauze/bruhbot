2d381e609e00b088c76f66595ed6c87ffe1c7846
BuildTools <unconfigured@null.spigotmc.org>
2023-06-14 19:09:40 -0700

    New files/edited files/updated gitignore

--- bruhbot legacy files/cogs/metalpipe/metalpipe.py
+++ bruhbot legacy files/cogs/metalpipe/metalpipe.py
@@ -3,13 +3,17 @@ from discord.ext.commands import Bot
 import discord
 import lavalink
 
+
 class Metalpipe(commands.Cog):
-    bot = Bot('$')
+    bot = Bot("$", intents=discord.Intents.default())
+
     @bot.event
     async def on_ready(self, bot):
-        await lavalink.initialize(bot, host='localhost', password='password', ws_port=2333)
-        
-    @commands.command(name='metalpipe')
+        await lavalink.initialize(
+            bot, host="localhost", password="password", ws_port=2333
+        )
+
+    @commands.command(name="metalpipe")
     async def metalpipe(self, ctx):
         guild = ctx.guild.id
         vc = ctx.author.voice.channel
@@ -19,9 +23,6 @@ class Metalpipe(commands.Cog):
             player = lavalink.get_player(guild)
         lavalink.Node.connect(self)
         user = self.bot.get_user(ctx.author.id)
-        track = await player.load_tracks('jixaw metal sound')
+        track = await player.load_tracks("jixaw metal sound")
         player.add(user, track)
         await player.play()
-
-def setup(bot):
-    bot.add_cog(Metalpipe())
