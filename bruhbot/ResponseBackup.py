be805967ba0c868caaeace8ca8182a5eb63493f3
BuildTools <unconfigured@null.spigotmc.org>
2024-03-28 23:35:52 -0700

    actually started backing up names.json and OPBR.json

--- bruhbot/ResponseBackup.py
+++ bruhbot/ResponseBackup.py
@@ -8,12 +8,23 @@ def run():
     bd = cwd + "backups\\"
     today = datetime.today()
     files = ["responses.txt", "data.json"]
+    cogfiles = ["names.json", "OPBR.json"]
     for file in files:
         src = cwd + file
         dst = f"{bd}{file}.backup[{today.strftime('%Y-%m-%d_%I-%M-%p')}]"  # YYYY-MM-DD + HH/MM (12 hour time)
         shutil.copyfile(src, dst)
         filelist = os.listdir(bd)
-        if len(filelist) > 20:  # max 10 of each
+        if len(filelist) > 20:  # max 5 of each
+            oldest = min(
+                filelist, key=lambda f: os.path.getmtime("{}/{}".format(bd, f))
+            )
+            os.remove(bd + oldest)
+    for file in cogfiles:
+        src = f"{cwd}cogs\\{file.replace('.json','')}\\{file}"
+        dst = f"{bd}{file}.backup[{today.strftime('%Y-%m-%d_%I-%M-%p')}]"  # YYYY-MM-DD + HH/MM (12 hour time)
+        shutil.copyfile(src, dst)
+        filelist = os.listdir(bd)
+        if len(filelist) > 20:  # max 5 of each
             oldest = min(
                 filelist, key=lambda f: os.path.getmtime("{}/{}".format(bd, f))
             )
