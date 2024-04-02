ae8a6675e044d95b17b76a7690ef773af620a635
BuildTools <unconfigured@null.spigotmc.org>
2024-04-01 21:51:42 -0700

    convert status to slash command

--- bruhbot/cogs/hd2/hd2.py
+++ bruhbot/cogs/hd2/hd2.py
@@ -66,9 +66,12 @@ class HD2(commands.Cog):
     async def hd(self, ctx):
         await self.update()
 
-    @commands.command()
-    @commands.cooldown(1, 300, commands.BucketType.guild)
-    async def status(self, ctx):
+    @app_commands.command(
+        name="hd2status",
+        description="Get current Galactic War status for Helldivers 2.",
+    )
+    @app_commands.checks.cooldown(1, 300, key=lambda i: (i.guild_id))
+    async def status(self, interaction: discord.Interaction):
         try:
 
             async def embed(
@@ -81,85 +84,95 @@ class HD2(commands.Cog):
                 embed = discord.Embed(color=color)
                 embed.title = name
                 embed.description = f"{owner} control"
-                embed.set_thumbnail(url="attachment://logo.png")
                 if owner == "Super Earth":
                     embed.add_field(name="Defense Campaign", value="")
+                    embed.set_thumbnail(url="attachment://selogo.png")
                 else:
                     embed.add_field(name="Liberation:", value=f"{liberation}%")
+                    if owner == "Automaton":
+                        embed.set_thumbnail(url="attachment://alogo.png")
+                    elif owner == "Terminid":
+                        embed.set_thumbnail(url="attachment://tlogo.png")
                 embed.set_footer(
                     text=f"{players} Helldivers", icon_url="attachment://hdlogo.png"
                 )
                 return embed
 
-            async with ctx.typing():
-                war_id = await self.get_war_id()
-                cresponse = get(f"{self.api}/{war_id}/status")
-                if cresponse.status_code == 200:
-                    planets = []
-                    planetdata = {}
-                    for c in cresponse.json()["campaigns"]:
-                        planets.append(c["planet"]["index"])
-                    for planet in planets:
-                        presponse = get(f"{self.api}/{war_id}/planets/{planet}/status")
-                        if presponse.status_code == 200:
-                            name = presponse.json()["planet"]["name"]
-                            lib = presponse.json()["liberation"]
-                            owner = presponse.json()["owner"]
-                            players = presponse.json()["players"]
-                            planetdata.update(
-                                {name: {"lib": lib, "owner": owner, "players": players}}
-                            )
-                        else:
-                            # owner = await self.bot.fetch_user(self.owner_id)
-                            await ctx.send(
-                                f"{presponse.status_code}\n{presponse.json()}"
-                            )
-                            return
+            await interaction.response.defer()
+            war_id = await self.get_war_id()
+            cresponse = get(f"{self.api}/{war_id}/status")
+            if cresponse.status_code == 200:
+                planets = []
+                planetdata = {}
+                for c in cresponse.json()["campaigns"]:
+                    planets.append(c["planet"]["index"])
+                for planet in planets:
+                    presponse = get(f"{self.api}/{war_id}/planets/{planet}/status")
+                    if presponse.status_code == 200:
+                        name = presponse.json()["planet"]["name"]
+                        lib = presponse.json()["liberation"]
+                        owner = presponse.json()["owner"]
+                        players = presponse.json()["players"]
+                        planetdata.update(
+                            {name: {"lib": lib, "owner": owner, "players": players}}
+                        )
+                    else:
+                        await interaction.followup.send(
+                            f"{presponse.status_code}\n{presponse.json()}",
+                            ephemeral=True,
+                        )
+                        return
+                embl = []
+                selogo = discord.File(
+                    f"{self.here}\\SuperEarth.png", filename="selogo.png"
+                )
+                alogo = discord.File(
+                    f"{self.here}\\Automaton.png", filename="alogo.png"
+                )
+                tlogo = discord.File(f"{self.here}\\Terminid.png", filename="tlogo.png")
+                hdlogo = discord.File(
+                    f"{self.here}\\Helldivers.png",
+                    filename="hdlogo.png",
+                )
+                for planet in planetdata:
+                    if planetdata[planet]["owner"] == "Humans":
+                        powner = "Super Earth"
+                        color = 0x05E7F3
+                    elif planetdata[planet]["owner"] == "Automaton":
+                        powner = "Automaton"
+                        color = 0xF11901
+                    elif planetdata[planet]["owner"] == "Terminids":
+                        powner = "Terminid"
+                        color = 0xFEE75C
+                    plib = (
+                        "DEFENSE CAMPAIGN"
+                        if powner == "Super Earth"
+                        else planetdata[planet]["lib"]
+                    )
+                    emb = await embed(
+                        planet,
+                        powner,
+                        plib,
+                        planetdata[planet]["players"],
+                        color,
+                    )
+                    embl.append(emb)
+                await interaction.followup.send(
+                    files=[selogo, alogo, tlogo, hdlogo], embeds=embl
+                )
 
-                    for planet in planetdata:
-                        if planetdata[planet]["owner"] == "Humans":
-                            powner = "Super Earth"
-                            img = f"{self.here}\\SuperEarth.png"
-                            color = 0x05E7F3
-                        elif planetdata[planet]["owner"] == "Automaton":
-                            powner = "Automaton"
-                            img = f"{self.here}\\Automaton.png"
-                            color = 0xF11901
-                        elif planetdata[planet]["owner"] == "Terminids":
-                            powner = "Terminid"
-                            img = f"{self.here}\\Terminid.png"
-                            color = 0xFEE75C
-                        plib = (
-                            "DEFENSE CAMPAIGN"
-                            if powner == "Super Earth"
-                            else planetdata[planet]["lib"]
-                        )
-                        logo = discord.File(img, filename="logo.png")
-                        hdlogo = discord.File(
-                            f"{self.here}\\Helldivers.png",
-                            filename="hdlogo.png",
-                        )
-                        emb = await embed(
-                            planet,
-                            powner,
-                            plib,
-                            planetdata[planet]["players"],
-                            color,
-                        )
-                        await ctx.send(files=[logo, hdlogo], embed=emb)
-
-                else:
-                    # owner = await self.bot.fetch_user(self.owner_id)
-                    await ctx.send(f"{cresponse.status_code}\n{cresponse.json()}")
-                    return
+            else:
+                await interaction.send(f"{cresponse.status_code}\n{cresponse.json()}")
+                return
         except Exception:
             owner = await self.bot.fetch_user(self.owner_id)
             await owner.send("Error logged in HD2.")
             ErrorLogger.run(traceback.format_exc())
 
     @status.error
-    async def status_error(self, ctx, error):
+    async def status_error(self, interaction: discord.Interaction, error):
         if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(
-                f"Command on cooldown, try again in {error.retry_after: .0f} seconds."
+            await interaction.response.send(
+                f"Command on cooldown, try again in {error.retry_after: .0f} seconds.",
+                ephemeral=True,
             )
