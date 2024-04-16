030c865cc6d6fe19dbb5b6ed7768f11bb2bce1a7
TheWizardofGauze <ian.strome@gmail.com>
2024-04-16 04:23:36 -0700

    misc

--- DupeFinder.py
+++ DupeFinder.py
@@ -1,13 +1,18 @@
 import json
+from multiprocessing import freeze_support
 import os
 import webbrowser
 
-from difPy import dif
+import difPy
 
-search = dif(r"C:\Users\Ian\Desktop\Folders\Redbot\images")
-jsonobj = json.dumps(search.result, indent=4)
-with open("DupeFinder.json", "w") as f:
-    f.write(jsonobj)
-webbrowser.open("DupeFinder.json")
-os.system("pause")
-os.remove("DupeFinder.json")
+
+if __name__ == "__main__":
+    freeze_support()
+    dif = difPy.build("C:/Users/Ian/Desktop/Folders/Redbot/images")
+    search = difPy.search(dif)
+    jsonobj = json.dumps(search.result, indent=4)
+    with open("DupeFinder.json", "w") as f:
+        f.write(jsonobj)
+    webbrowser.open("DupeFinder.json")
+    os.system("pause")
+    os.remove("DupeFinder.json")
