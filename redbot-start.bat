d6dfb4d3e848adefcc7dfc413fccc62d36c474e1
TheWizardofGauze <ian.strome@gmail.com>
2024-04-21 21:23:00 -0700

    misc

--- redbot-start.bat
+++ redbot-start.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0
-echo python -m pip install -U Red-DiscordBot
+python -m pip install -U Red-DiscordBot
 echo python -m pip install -U discord
 start "" cmd /c cscript redbot.vbs
