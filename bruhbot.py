6c345da2bf51ffb95fd42be4a085791f9b68b288
TheWizardofGauze <72229519+TheWizardofGauze@users.noreply.github.com>
2025-01-17 19:34:36 -0800

    Simplify regex in on_message

--- bruhbot.py
+++ bruhbot.py
@@ -104,8 +104,7 @@ async def on_message(msg):
         ctx = await bot.get_context(msg)
         if ctx.author.bot or ctx.channel.id == ignored_channel:
             return
-        notag = re.sub(r"<@.*?>", "", msg.content)
-        nospace = re.sub("[^a-zA-Z0-9]", "", notag).lower()
+        nospace = re.sub("<@.*?>|[^a-zA-Z0-9]", "", msg.content).lower()
         name = re.sub("[^a-zA-Z0-9]", "", ctx.me.display_name).lower()
         top_role = str(ctx.guild.get_member(bot.user.id).top_role.id) if ctx.guild else None
 
