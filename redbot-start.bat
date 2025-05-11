5b5fba47597c3c65b11cbe02b60d40f5c7396deb
TheWizardofGauze <72229519+TheWizardofGauze@users.noreply.github.com>
2025-05-10 20:47:28 -0700

    misc

--- redbot-start.bat
+++ redbot-start.bat
@@ -1,5 +1,4 @@
 @echo off
 pushd %~dp0
 python -m pip install -U Red-DiscordBot
-echo python -m pip install -U discord
 start "" cmd /c cscript redbot.vbs
--- redbot.bat
+++ redbot.bat
@@ -1,1 +1,12 @@
+@ECHO OFF
+::START
+::python -m pip install -U Red-DiscordBot
 python -O -m redbot nword
+
+::IF %ERRORLEVEL% == 1 GOTO RESTART
+::IF %ERRORLEVEL% == 26 GOTO RESTART
+::EXIT /B %ERRORLEVEL%
+
+::RESTART
+::ECHO Restarting...
+::GOTO START
